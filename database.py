01fb457ca9377de3f29340b9947e3dfafac1bf7d
Sebastian Candia <candiamarins@gmail.com>
2026-04-09 16:26:34 -0500

    Creación de tabla estudiantes

--- database.py
+++ database.py
@@ -2,3 +2,15 @@ import sqlite3
 
 def conectar():
     return sqlite3.connect("estudiantes.db")
+
+def crear_tabla(conn):
+
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS estudiantes(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT,
+        correo TEXT,
+        nota REAL
+    )
+    """)
+    conn.commit()
